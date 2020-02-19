1feb074110d867f2d5e57d1a0658cabfcebf5d7a
Akash Bhave <akashbbhave@gmail.com>
2020-02-19 15:59:12 -0500

    Create data file, expose two endpoints

---
+++ server/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+data = json.load(open('data.json', 'r'))
+
+
+@app.route("/")
+def handle_root():
+    return "test"
+
+
+@app.route("/items")
+def handle_items():
+    return jsonify(data['items'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
